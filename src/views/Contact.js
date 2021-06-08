c1d2a3f9cf134d99d33bd3ee94c0a74c7008f54e
Nicole Colabella <ncolabella@fubo.tv>
2021-06-08 09:40:12 -0400

    update contact page for mobile

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,5 +1,7 @@
 
 import styled from 'styled-components';
+import { isMobileOnly } from 'react-device-detect';
+
 import topCap from '../svgs/topcap.svg';
 import contactInfo from '../svgs/contact-info.svg';
 
@@ -17,9 +19,17 @@ const Container = styled.div`
   }
 
   .topcap {
-    margin: 0px auto 25px;
+    margin: ${isMobileOnly ? '0px auto' : '0px auto 10px'};
     display: block;
   }
+
+  ${isMobileOnly && `
+    .contact {
+      width: 100%;
+    }
+  `}
+
+  
 
   p {
     width: 100%;
@@ -37,7 +47,7 @@ function Contact({
     <Container {...props}>
       <h1>Contact</h1>
       <img className="topcap" src={topCap} alt="Applications, Programming, Interfaces" />
-      <img src={contactInfo} alt="contact information, in SVG path form to avoid scraping. For visually impaired readers, please email m a t at the website domain listed."/>
+      <img className="contact" src={contactInfo} alt="contact information, in SVG path form to avoid scraping. For visually impaired readers, please email m a t at the website domain listed."/>
       <p>Email is preferred, most messages responded to within 24 to 48 hours.</p>
 
     </Container>
